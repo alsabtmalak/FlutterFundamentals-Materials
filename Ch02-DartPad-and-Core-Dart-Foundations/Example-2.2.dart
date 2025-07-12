4c6f662f47ef73db867efa31efbcf23c0db59d3f
ma11111ak <alsabtmalak5@gmail.com>
2025-07-12 16:05:08 +0300

    Example 2.2

---
+++ Ch02-DartPad-and-Core-Dart-Foundations/Example-2.2.dart
@@ -0,0 +1,20 @@
+/* Example 2.2 var Infers the Type */
+
+void main() {
+  var x5 = 5; // `var` => int
+  print(x5);
+  print(x5.runtimeType); // int
+  print('');
+
+  // x5 = 5.5; // Error: cannot assign double to int
+
+  var x6 = 7.5; // `var` => double
+  print(x6);
+  print(x6.runtimeType); // double
+  print('');
+
+  var x7 = 'hello csis 401';
+  print(x7);
+  print(x7.runtimeType); // String
+  print('');
+}
