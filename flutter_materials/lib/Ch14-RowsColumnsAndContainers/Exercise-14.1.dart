685d7da45d60236ed7a62d6d539e3d61c358914c
ma11111ak <alsabtmalak5@gmail.com>
2025-09-16 05:00:34 +0300

    Chapter 14 Examples & Exercises

---
+++ flutter_materials/lib/Ch14-RowsColumnsAndContainers/Exercise-14.1.dart
@@ -0,0 +1,7 @@
+/* Exercise 14.1. 
+
+Goal: Practice using a Row of Column widgets with text formatting and spacing.
+
+  •	Each column has two Text widgets: number on top, label below.
+  •	Use MainAxisAlignment.spaceEvenly in the Row.
+  •	Use bold and font size variations for numbers. */
--- flutter_materials/lib/main.dart
+++ flutter_materials/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Ch12-ImagesInFlutter/Example-12.3.1.dart';
+import 'Ch14-RowsColumnsAndContainers/Example-14.2.dart';
 
 void main() {
-  runApp(Example12_3_1());
+  runApp(Example14_2());
 }
